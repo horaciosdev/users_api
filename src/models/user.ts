06771074fa6d8f8a6bdd885745211db6ebc3ebdf
Horacio <horacio.schumann@gmail.com>
2023-01-28 11:20:31 -0300

    feat: add id to User

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,7 @@
 export interface User {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 }
